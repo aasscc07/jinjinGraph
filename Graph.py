fd719525b5c5c95a5b42ec6e834c30b30f0e756a
aasscc07 <75631159+aasscc07@users.noreply.github.com>
2023-05-24 17:18:31 +0900

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import math
 
 
 pygame.init()
@@ -10,15 +11,34 @@ WINDOW_HEIGHT = 600
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Graph")
 
-# Lines = pygame.Rect()
-x = 0
-# y1 = x ** 2 + 2 * x + 1
 
-# y2 = x + 1
 
-def F_Y2(x):
-    return x + 1
 
+
+
+n = int(input("최고차항 : "))
+xn_ = []
+for i in range(n + 1):
+    v1 = int(input(f"x{i} : "))
+    xn_.append(v1)
+
+def Functions(x,xn :list):
+    x_ = 0
+    sum_y = 0
+    for i in range(len(xn)):
+        
+        x_ = xn[i] * (x ** i) 
+        
+        sum_y += x_
+
+    return -1 * sum_y
+
+
+        
+            
+    
+
+    
 C_BLACK = (0,0,0)
 C_RED = (255,0,0)
 while True:
@@ -26,10 +46,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        
-
-
-    
     screen.fill((255,255,255))
     for i in range(31):
         if i == 15:
@@ -42,14 +58,25 @@ while True:
         else:
             pygame.draw.line(screen,C_BLACK, [0,i * 20],[WINDOW_HEIGHT,i * 20],1)
 
-    for i in range(600):
-        pygame.draw.circle(screen,C_RED,[x + 300,F_Y2(x) + 300],1,1)
-        x += 0.1
 
-        
+   
+    
+    # a = 함수 그래프
+    def draw_fuction():
+        x1 = 0
+        x2 = 0
+        for i in range(10000):
+            pygame.draw.circle(screen,C_RED,[x1 + 300, Functions(x1 / 20,xn_) * 20 + 300],1,1)
+            pygame.draw.circle(screen,C_RED,[x2 + 300, Functions(x2 / 20,xn_) * 20 + 300],1,1)
+            
+            x1 += 0.1
+            x2 -= 0.1
+
+
+    # draw_fuction
 
     
-
+    draw_fuction()
 
     
 
